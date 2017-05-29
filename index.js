8db0240f0efa3cf090d87e8ea790c7cb667a2fc6
Sarah Schütz <sarahschuetz@gmx.at>
2017-05-29 13:36:29 +0200

    ADD movement

--- index.js
+++ index.js
@@ -3,15 +3,25 @@ const board = new five.Board();
 
 board.on('ready', function ready() {
   let switchFlipped = true;
-  this.analogRead(2, (data) => {
-    if (switchFlipped !== data > 500) {
-      switchFlipped = !switchFlipped;
-      console.log('switch flipped!', switchFlipped ? 'On' : 'Off');
-    }
-  });
+
   const servo = new five.Servo({
     pin: 8,
     center: true
   });
-  servo.to(180);
+
+  this.analogRead(2, (data) => {
+
+    if (switchFlipped !== data > 500) {
+      switchFlipped = !switchFlipped;
+
+      if(switchFlipped) {
+        servo.to(180);
+        setTimeout(() => {servo.to(0);}, 1000);
+      }
+
+      console.log('switch flipped!', switchFlipped ? 'On' : 'Off');
+    }
+  });
+
+  servo.to(0);
 });
